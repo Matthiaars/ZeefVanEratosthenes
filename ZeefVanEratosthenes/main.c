20725325728ca5dd97d068d0b6d6e0c1710dd765
Matthias Lemmens <matthias.lemmens@gmail.com>
2012-05-27 17:09:31 +0200

    Initial Commit

---
+++ ZeefVanEratosthenes/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ZeefVanEratosthenes
+//
+//  Created by Matthias Lemmens on 27/05/12.
+//  Copyright (c) 2012 Lemmens inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
