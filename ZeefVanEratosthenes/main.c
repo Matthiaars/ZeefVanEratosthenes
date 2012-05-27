7fd6fae02f9090d0fb90e39970766425e1294605
Matthias Lemmens <matthias.lemmens@gmail.com>
2012-05-27 17:19:54 +0200

    first commit

--- ZeefVanEratosthenes/main.c
+++ ZeefVanEratosthenes/main.c
@@ -1,18 +1,83 @@
-//
-//  main.c
-//  ZeefVanEratosthenes
-//
-//  Created by Matthias Lemmens on 27/05/12.
-//  Copyright (c) 2012 Lemmens inc. All rights reserved.
-//
+/******************************************************
+ main.c
+ ZeefVanEratosthenes
+ 
+ Created by Matthias Lemmens on 10/10/11.
+ Thanks to Pieter Gunst
+ 
+ Copyright (c) 2011 Lemmens inc. All rights reserved.
+ ******************************************************/
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void printZeef (int* p, int length)
+{
+    int getal;
+    
+    printf ("\n\n     ");
+    
+    for (getal=2; getal<=length; getal++)
+    {
+        if (p[getal]==0)
+        {
+            if (getal<10)
+            {
+                printf("|0%d| ", getal);
+            } else {
+                printf("|%d| ", getal);
+            }
+        } else {
+            if (getal<10)
+            {
+                printf(" 0%d  ", getal);
+            } else {
+                printf(" %d  ", getal);
+            }
+        }
+        
+        if (getal%(int)(sqrt(length))==0)
+        {
+            printf("\n");
+        }
+    }
+    
+    printf("\n\n");
+}
+
+void zeef (int x)
+{
+    
+    int length=x, getal, veelvoud;
+    int* list =(int*) calloc(x, sizeof(int));
+    
+    for (getal=2; getal<=sqrt(length); getal++)
+    {
+        if (list[getal]==0)
+        {
+            for (veelvoud=(2*getal); veelvoud<=length; veelvoud+=getal)
+            {
+                list[veelvoud]=1;
+            }
+        }
+    }
+    
+    printZeef(list, x);
+}
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    int length;
+    
+    printf("Hello, Ladies! Give me a number and I'll show you Eratosthenes' sieve like no man has ever done before!\n");
+    
+    printf("Enter your number;  ");
+    scanf("%d", &length);
+    
+    length=(int) length;
+    zeef(length);
+    
     return 0;
 }
 
